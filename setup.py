34b97e3559dc16b4ee730fe7d2c51b91e4a2abaa
Samuel Paul Grant <Samuel Grant>
2025-04-11 11:35:30 -0500

    Adding package setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pyutils',
+    version='0.0.0',
+    author='Sophie Middleton, Samuel Grant',
+    description='Python utilities for Mu2e collaborators.', 
+    url='https://github.com/sam-grant/pyutils',
+    packages=['pyutils']
+)
