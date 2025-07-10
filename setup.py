76faeaa8fc83215144389e91a93d0fdef43ac66b
sam-grant <samuel.grant94@gmail.com>
2025-07-10 16:45:50 +0000

    Incremented minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyutils",
-    version="1.0.0",
+    version="1.1.0",
     author="Sophie Middleton, Samuel Grant, Andrew Edmonds, Leo Borrel",
     description="Python tools for Mu2e collaborators",
     url="https://github.com/Mu2e/pyutils",
